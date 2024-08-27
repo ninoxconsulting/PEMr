80d7440b0ccb07cf0381b63cbb8746d9d736702e
Andy Teucher <andy.teucher@gmail.com>
2024-08-27 09:44:40 -0700

    Simplify run_pemr to just load packages, as installation is taken care of by DESCRIPTION

--- R/run_pemr.R
+++ R/run_pemr.R
@@ -1,31 +1,14 @@
-#' Run PEMr libraries
+#' Attach PEMr packages
 #'
-#' Set up required libraries to complete Predictive Ecosystem Mapping. Checks first if the required libraires exist and downloads if needed.
-#'
+#' Calling `run_pemr()` will attach all of the PEMr family of packages: `PEMprepr`,
+#' `PEMsamplr`, and `PEMmodelr`.
+#' 
 #' @return TRUE or FALSE
 #' @export
 #' @author Genevieve Perkins
 #' @examples
 #' run_pemr()
-
 run_pemr <- function(){
-
-  #library(PEMprepr)
-  #library(PEMsamplr)
-  #library(PEMmapr)
-
-  packages <- c("PEMprepr", "PEMsamplr", "PEMmodelr")
-
-  missing <- setdiff(packages, rownames(installed.packages()))
-
-  for(i in missing){
-
-    remotes::install_github("Rfast", build_vignettes = FALSE)
-    tpackage <- paste0("bcgov/",i)
-    remotes::install_github(tpackage, build_vignettes = FALSE)
-
-    return(TRUE)
-  }
 
   library(PEMprepr)
   library(PEMsamplr)
