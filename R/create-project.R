c10fd40c128fd6cd9b54a23797ddca62464b4df6
Andy Teucher <andy.teucher@gmail.com>
2024-09-05 16:20:43 -0700

    style

--- R/create-project.R
+++ R/create-project.R
@@ -31,33 +31,32 @@
 #' create_pemr_project()
 #' }
 create_pemr_project <- function(
-  path = ".",
-  aoi_name,
-  aoi_file = NULL,
-  rstudio = rstudioapi::isAvailable(),
-  open = rlang::is_interactive()
-) {
+    path = ".",
+    aoi_name,
+    aoi_file = NULL,
+    rstudio = rstudioapi::isAvailable(),
+    open = rlang::is_interactive()) {
   path <- fs::path(path, aoi_name)
-  
+
   # force early evaluation of file path before we switch projects
   force(aoi_file)
-  
+
   # Create project but don't open yet as we need to add the infrastructure
   project_path <- usethis::create_project(path, rstudio = rstudio, open = FALSE)
-  
+
   # Activate the new project directory inside the scope of this function
   # so we can easily do stuff inside it
   usethis::local_project(project_path)
-  
+
   ## Add the project-specific infrastructure
   project_dirs <- create_directories()
-  
+
   write_core_files(
     list(aoi_name = aoi_name)
   )
-  
+
   fid <- make_fid(project_dirs)
-  
+
   aoi_dest_path <- fid$dir_0010_vector$path_abs
   if (!is.null(aoi_file)) {
     if (file.exists(aoi_file)) {
@@ -65,10 +64,10 @@ create_pemr_project <- function(
     } else {
       cli::cli_warn("{.path {aoi_file}} does not exist. Proceeding to set up project without copying it. Please place it manually in {.path {aoi_dest_path}}")
     }
-    } else {
+  } else {
     cli::cli_alert_warning("No AOI file specified. You will need to copy an AOI file to {.path {aoi_dest_path}}")
   }
-  
+
   if (open) {
     if (usethis::proj_activate(usethis::proj_get())) {
       # working directory/active project already set; clear the scheduled
@@ -76,19 +75,18 @@ create_pemr_project <- function(
       withr::deferred_clear()
     }
   }
-  
+
   invisible(usethis::proj_get())
 }
 
 create_directories <- function(
-    file = fs::path_package("PEMr", "extdata/directory_structure.csv")
-) {
+    file = fs::path_package("PEMr", "extdata/directory_structure.csv")) {
   dir_df <- utils::read.csv(file)
-  
+
   project_dirs <- fs::path(
     dir_df$base_dir, dir_df$subdir_1, dir_df$subdir_2, dir_df$subdir_3
   )
-  
+
   lapply(c(project_dirs, "_meta"), usethis::use_directory)
   project_dirs
 }
@@ -103,20 +101,20 @@ write_core_files <- function(data) {
 
 make_fid <- function(dirs) {
   fid <- as.list(dirs)
-  
+
   names(fid) <- paste0(
     "dir_", # so doesn't start with a number
     gsub("[.-/_a-zA-Z]", "", fs::path_dir(dirs)), # only numbers for base baths
     fs::path_file(dirs) # full dirname for terminal directory
   )
-  
+
   fid <- lapply(fid, function(x) {
     list(
       "path_rel" = fs::path(x[1]),
       "path_abs" = fs::path_abs(x[1])
     )
   })
-  
+
   saveRDS(fid, fs::path("_meta", "fid.rds"))
   fid
 }
